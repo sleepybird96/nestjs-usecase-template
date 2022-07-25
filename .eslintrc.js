c1de51913b5e9f982c29b23b61da3fbb179afe5f
Jisang Park <iam@park.jisang.dev>
2022-07-25 16:32:42 +0900

    Add lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,17 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import', 'spellcheck'],
   extends: [
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
     'plugin:prettier/recommended',
+    'prettier',
   ],
   root: true,
   env: {
@@ -17,9 +21,37 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    // Default
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    // Custom
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    'spellcheck/spell-checker': [
+      1,
+      {
+        lang: 'en_US',
+        skipWords: [
+          'ack',
+          'cors',
+          'dto',
+          'enum',
+          'io',
+          'joi',
+          'namespace',
+          'nestjs',
+          'nullable',
+          'orm',
+          'postgresql',
+          'redis',
+          'mq',
+          'kafka',
+        ],
+        skipIfMatch: ['http://[^s]*'],
+        minLength: 3,
+      },
+    ],
   },
 };
