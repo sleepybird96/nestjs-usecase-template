6155ece199a8941d3180d4b2003d63e27322d0a6
Jisang Park <iam@park.jisang.dev>
2022-07-25 17:26:23 +0900

    Add ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
